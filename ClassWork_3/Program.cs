bfea6fd288e938b0e5d29c45e2fa7de9b7c2ffa9
zorgensk <zorgensk@gmail.com>
2022-11-27 21:16:47 +0700

    ClassWork3

---
+++ ClassWork_3/Program.cs
@@ -0,0 +1,10 @@
+﻿    // Перевод из десятичной в двоичную
+    Console.Write("Введите число: ");
+    int a = int.Parse(Console.ReadLine());
+    string dvoich = "";
+    while (a > 0)
+    {
+        dvoich = (a % 2).ToString() + dvoich;
+        a /= 2;
+    }
+    Console.WriteLine(dvoich);
