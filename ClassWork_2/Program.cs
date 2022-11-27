8f1bcc7f61c3f5e6e743a5ef95b3656e6d0ca958
zorgensk <zorgensk@gmail.com>
2022-11-27 21:11:34 +0700

    ClassWork2

---
+++ ClassWork_2/Program.cs
@@ -0,0 +1,16 @@
+﻿  Console.Write("Введите число: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    
+    int N_1 = 1;
+    int N_2 = 0;
+
+    Console.Write($"{N_2} {N_1}");
+
+    for (int i = 1; i < N - 1; i++)
+    {
+        Console.Write($" {N_2 + N_1}");
+        int b = N_1;
+        N_1 = N_2 + N_1;
+        N_2 = b;
+    }
+    Console.WriteLine("\n\n");
