29ad03b014ad32deac6d117337249c05deaef16e
zorgensk <zorgensk@gmail.com>
2022-12-04 17:24:43 +0700

    Homework2

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -13,7 +13,7 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 double k2 = Convert.ToInt32(Console.ReadLine());
 
 if((k1 == k2) && (b1 == b2)) Console.WriteLine("Прямые совпадают!");
- else if (k1 == k2) Console.WriteLine("Прямые параллельны!")ж
+ else if (k1 == k2) Console.WriteLine("Прямые параллельны!");
 
 double x = (b2 - b1)/(k1 - k2);
 double y = k1 * x + b1;
